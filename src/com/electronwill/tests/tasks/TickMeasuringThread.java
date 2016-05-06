0b2a589db48c43c0fa637066616536f52667c11c
TheElectronWill <TheElectronWill@users.noreply.github.com>
2016-05-06 17:56:37 +0200

    Print statistics on exit.

--- src/com/electronwill/tests/tasks/TickMeasuringThread.java
+++ src/com/electronwill/tests/tasks/TickMeasuringThread.java
@@ -1,7 +1,9 @@
 package com.electronwill.tests.tasks;
 
 import com.electronwill.tests.thread.NiceThread;
+import java.util.DoubleSummaryStatistics;
 import java.util.concurrent.CountDownLatch;
+import java.util.stream.DoubleStream;
 
 /**
  *
@@ -13,6 +15,7 @@ public class TickMeasuringThread extends NiceThread {
 	final int nTasks;
 	volatile boolean tickFinished = true;
 	volatile long t0;
+	final DoubleStream.Builder builder = DoubleStream.builder();
 
 	public TickMeasuringThread(int nTasks) {
 		super("TickMeasuringThread");
@@ -22,6 +25,7 @@ public class TickMeasuringThread extends NiceThread {
 
 	@Override
 	public void doRun() throws InterruptedException {
+		Runtime.getRuntime().addShutdownHook(new PrintAverage());
 		while (run) {
 			latch.await();
 			long t1 = System.nanoTime();
@@ -32,6 +36,50 @@ public class TickMeasuringThread extends NiceThread {
 			double ms = (t1 - t0) / 1000_000.0;
 			System.out.println("tick : " + ms + " millisecondes");
 		}
+	}
+
+	private class PrintAverage extends Thread {
+
+		@Override
+		public void run() {
+			run = false;
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException ex) {
+				ex.printStackTrace();
+			}
+			DoubleStream stream = builder.build().skip(3);
+			AdvancedStatistics stats = new AdvancedStatistics();
+			stream.forEach(stats::accept);
+			System.out.println("---------- Stats ----------");
+			System.out.println("Les 3 premières mesures ne comptent pas.");
+			System.out.println("Nombre de mesures prises en compte : " + stats.getCount());
+			System.out.println("Durée d'un tick :");
+			System.out.println("moyenne : " + stats.getAverage());
+			System.out.println("maximum : " + stats.getMax());
+			System.out.println("minimum : " + stats.getMin());
+			System.out.println("écart-type : " + stats.getStandardDeviation());
+		}
+	}
+
+	private class AdvancedStatistics extends DoubleSummaryStatistics {
+
+		private double sumOfSquares = 0;
+
+		@Override
+		public void accept(double value) {
+			super.accept(value);
+			sumOfSquares += (value * value);
+		}
+
+		public double getVariance() {
+			return sumOfSquares / getCount() - (getAverage() * getAverage());
+		}
+
+		public double getStandardDeviation() {
+			return Math.sqrt(getVariance());
+		}
+
 	}
 
 }
